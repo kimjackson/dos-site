2564d78d66a41128d72cda11a0784c2caf8ed094
Kim Jackson <kjackson@acl.arts.usyd.edu.au>
2009-05-12 16:06:13 +1000

    Cleaned up highlight.js - passes JSLint fairly cleanly; works in IE.

--- js/highlight.js
+++ js/highlight.js
@@ -1,14 +1,36 @@
+/*jslint browser: true, eqeqeq: true, immed: true, newcap: true, undef: true, white: true, indent: 4*/
+/*global window, Node, $, YAHOO */
+
+if (! window.Node) {
+	/* courtesy of: http://safalra.com/web-design/javascript/dom-node-type-constants/ */
+	var Node = {
+		ELEMENT_NODE                :  1,
+		ATTRIBUTE_NODE              :  2,
+		TEXT_NODE                   :  3,
+		CDATA_SECTION_NODE          :  4,
+		ENTITY_REFERENCE_NODE       :  5,
+		ENTITY_NODE                 :  6,
+		PROCESSING_INSTRUCTION_NODE :  7,
+		COMMENT_NODE                :  8,
+		DOCUMENT_NODE               :  9,
+		DOCUMENT_TYPE_NODE          : 10,
+		DOCUMENT_FRAGMENT_NODE      : 11,
+		NOTATION_NODE               : 12
+	};
+}
+
 var currentRefs;
 
-function highlight (root, refs) {
+function highlight(root, refs) {
 	// normalise addresses
-	for (var i = 0; i < refs.length; ++i) {
+	var i, j;
+	for (i = 0; i < refs.length; ++i) {
 
 		if (! refs[i].endElems  ||  ! refs[i].endElems.length) {
 			refs[i].endElems = refs[i].startElems;
 		}
 		else {
-			for (var j = 0; j < refs[i].startElems.length; ++j) {
+			for (j = 0; j < refs[i].startElems.length; ++j) {
 				if (j < refs[i].endElems.length  &&  refs[i].endElems[j] === null) {
 					refs[i].endElems[j] = refs[i].startElems[j];
 				}
@@ -20,49 +42,55 @@ function highlight (root, refs) {
 		}
 	}
 
-	//console.log("normalised refs: " + refs.toSource());
 	currentRefs = [];
 	traverse(root, refs, []);
 }
 
-function traverse (elem, refs, address) {
-	var startingRefs = [];
-	var endingRefs = [];
+function traverse(elem, refs, address) {
+	var i,
+		j,
+		skip,
+		match,
+		startingRefs = [],
+		endingRefs = [],
+		children = [],
+		elemNumber = 0,
+		wordOffset = 0;
 
 	// find refs starting at this elem
-	for (var i = 0; i < refs.length; ++i) {
+	for (i = 0; i < refs.length; ++i) {
 		// only check refs which are not already current
-		var skip = false;
-		for (var j = 0; j < currentRefs.length; ++j) {
-			if (currentRefs[j] == refs[i]) skip = true;
+		skip = false;
+		for (j = 0; j < currentRefs.length; ++j) {
+			if (currentRefs[j] === refs[i]) {
+				skip = true;
+			}
 		}
 		if (! skip) {
-			var match = checkAddress(address, refs[i].startElems);
-			if (match) startingRefs.unshift(i);
+			match = checkAddress(address, refs[i].startElems);
+			if (match) {
+				startingRefs.unshift(i);
+			}
 		}
 	}
 
 	// find refs ending at this elem
-	for (var i = 0; i < refs.length; ++i) {
-		var match = checkAddress(address, refs[i].endElems);
-		if (match) endingRefs.unshift(i);
+	for (i = 0; i < refs.length; ++i) {
+		match = checkAddress(address, refs[i].endElems);
+		if (match) {
+			endingRefs.unshift(i);
+		}
 	}
 
-//console.log("traverse " + address.join(",") + (startingRefs.length ? " startingRefs: " + startingRefs.join(",") : "")
-//											+ (endingRefs.length ? " endingRefs: " + endingRefs.join(",") : "")
-//											+ (currentRefs.length ? " currentRefs: " + currentRefs.join(",") : ""));
-
-
-
 	// copy childNodes - more children might be inserted as we go!
-	var children = [];
-	for (var i = 0; i < elem.childNodes.length; ++i) {
+	children = [];
+	for (i = 0; i < elem.childNodes.length; ++i) {
 		children.push(elem.childNodes[i]);
 	}
 
-	var elemNumber = 0;
-	var wordOffset = 0;
-	for (var i = 0; i < children.length; ++i) {
+	elemNumber = 0;
+	wordOffset = 0;
+	for (i = 0; i < children.length; ++i) {
 		if (children[i].nodeType === Node.ELEMENT_NODE) {
 			// may alter currentRefs!
 			wordOffset += traverse(children[i], refs, address.concat([++elemNumber]));
@@ -76,48 +104,76 @@ function traverse (elem, refs, address) {
 	return wordOffset;
 }
 
-// check currrent location against given address
-function checkAddress (currLoc, addr) {
+function checkAddress(currLoc, addr) {
+	// check currrent location against given address
 	var i;
 
-	if (currLoc.length !== addr.length) return false;
+	if (currLoc.length !== addr.length) {
+		return false;
+	}
 
 	for (i = 0; i < addr.length; ++i) {
-		if (addr[i] === null) break;
-		if (currLoc.length <= i) return false;
-		if (currLoc[i] !== addr[i]) return false;
+		if (addr[i] === null) {
+			break;
+		}
+		if (currLoc.length <= i) {
+			return false;
+		}
+		if (currLoc[i] !== addr[i]) {
+			return false;
+		}
 	}
 
 	return true;
 }
 
-function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
-//console.log("transformElement:" + " currentRefs: " + currentRefs.join(",")
-//								+ " startingRefs: " + startingRefs.join(",")
-//								+ " endingRefs: " + endingRefs.join(","));
-	var parentElem = elem.parentNode;
-	var text = elem.textContent;
-	text = text.replace(/^\s+/, "").replace(/\s+$/, "");
-	var words = text.split(/\s+/);
+function transformTextNode(elem, refs, startingRefs, endingRefs, wordOffset) {
+	var parentElem = elem.parentNode,
+		text = elem.nodeValue,
+		words,
+		i,
+		j,
+		skip,
+		s,
+		myStartingRefs = [],
+		myEndingRefs = [],
+		ref,
+		a,
+		newElements = [],
+		startPositions = [],
+		endPositions = [],
+		refId,
+		startWord,
+		endWord,
+		sortfunc,
+		refStack = [],
+		sections = [],
+		section,
+		w,
+		startPos,
+		endPos,
+		newRefStack,
+		wordString,
+		newState,
+		remove;
 
-	var myStartingRefs = [];
-	var myEndingRefs = [];
+
+	text = text.replace(/^\s+/, "").replace(/\s+$/, "");
+	words = text.split(/\s+/);
 
 	// filter just the applicable refs from startingRefs and endingRefs
-	for (var i = 0; i < startingRefs.length; ++i) {
-		var s = refs[startingRefs[i]].startWord;
+	for (i = 0; i < startingRefs.length; ++i) {
+		s = refs[startingRefs[i]].startWord;
 		if (wordOffset < s  &&  s <= (wordOffset + words.length)) {
 			myStartingRefs.push(startingRefs[i]);
 		}
 	}
-	for (var i = 0; i < endingRefs.length; ++i) {
-		var s = refs[endingRefs[i]].endWord;
+	for (i = 0; i < endingRefs.length; ++i) {
+		s = refs[endingRefs[i]].endWord;
 		if (wordOffset < s  &&  s <= (wordOffset + words.length)) {
 			myEndingRefs.push(endingRefs[i]);
 		}
 	}
-
-//console.log("myStartingRefs: " + myStartingRefs.join(",") + " myEndingRefs: " + myEndingRefs.join(","));
 
 	// trivial trivial case: there is nothing happening in this text node.
 	// we only visited it to find out how many words it contains
@@ -128,69 +184,66 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 	// trivial case: there are no references starting or ending within this element
 	// so just highlight it and link to the ref on the top of the currentRefs stack
 	if (! myStartingRefs.length  &&  ! myEndingRefs.length) {
-		var ref = refs[currentRefs[0]];
-		var a = document.createElement("a");
+		ref = refs[currentRefs[0]];
+		a = document.createElement("a");
 		a.className = (currentRefs.length > 1 ? "annotation multiple" : "annotation");
 		a.href = "#ref=" + ref.recordID;
 		a.title = ref.title;
 		a.name = "ref" + ref.recordID;
 		a.setAttribute("annotation-id", ref.recordID);
-		a.onclick = function() { highlightAnnotation(this.getAttribute("annotation-id")); return false; };
-		a.innerHTML = elem.textContent;
+		a.onclick = function () {
+			highlightAnnotation(this.getAttribute("annotation-id"));
+			return false;
+		};
+		a.innerHTML = elem.nodeValue;
 		elem.parentNode.replaceChild(a, elem);
 
 	} else {
 
-		var newElements = [];
-
-		var startPositions = [];
-		var endPositions = [];
-
-		for (var i = 0; i < currentRefs.length; ++i) {
-			startPositions.push( { "word": 1, "refId": currentRefs[i] } );
+		for (i = 0; i < currentRefs.length; ++i) {
+			startPositions.push({ "word": 1, "refId": currentRefs[i] });
 			// if this ref is in myEndingRefs, ignore it here -- it will be entered into the endPositions list correctly below
-			var skip = false;
-			for (var j = 0; j < myEndingRefs.length; ++j) {
-				if (myEndingRefs[j] == currentRefs[i]) skip = true;
+			skip = false;
+			for (j = 0; j < myEndingRefs.length; ++j) {
+				if (myEndingRefs[j] === currentRefs[i]) {
+					skip = true;
+				}
 			}
-			if (! skip)
-				endPositions.push( { "word": words.length, "refId": currentRefs[i] } );
+			if (! skip) {
+				endPositions.push({ "word": words.length, "refId": currentRefs[i] });
+			}
 		}
-		for (var i = 0; i < myStartingRefs.length; ++i) {
-			var refId = myStartingRefs[i];
-			var startWord = refs[refId].startWord - wordOffset;
-			startPositions.push( { "word": startWord, "refId": refId, "starting": true } );
+		for (i = 0; i < myStartingRefs.length; ++i) {
+			refId = myStartingRefs[i];
+			startWord = refs[refId].startWord - wordOffset;
+			startPositions.push({ "word": startWord, "refId": refId, "starting": true });
 		}
-		for (var i = 0; i < myEndingRefs.length; ++i) {
-			var refId = myEndingRefs[i];
-			var endWord = refs[refId].endWord - wordOffset;
-			endPositions.push( { "word": endWord, "refId": refId, "ending": true } );
+		for (i = 0; i < myEndingRefs.length; ++i) {
+			refId = myEndingRefs[i];
+			endWord = refs[refId].endWord - wordOffset;
+			endPositions.push({ "word": endWord, "refId": refId, "ending": true });
 		}
 
-		var sortfunc = function(a,b) {
-			if (a.word == b.word)
+		sortfunc = function (a, b) {
+			if (a.word === b.word) {
 				return a.refId - b.refId;
-			else
+			} else {
 				return a.word - b.word;
+			}
 		};
 
 		startPositions = startPositions.sort(sortfunc);
 		endPositions = endPositions.sort(sortfunc);
 
-//console.log("startPositions:" + startPositions.toSource());
-//console.log("endPositions:" + endPositions.toSource());
-
-		var refStack = [];
-		var sections = [];
-		var section = { "startWord": 1, "endWord": null, "refId": null, "refCount": 0, "refNums": [] };
-		for (var w = 1; w <= words.length; ++w) {
-			var startPos = null;
+		section = { "startWord": 1, "endWord": null, "refId": null, "refCount": 0, "refNums": [] };
+		for (w = 1; w <= words.length; ++w) {
+			startPos = null;
 			// push any refs that start at this pos onto the stack
-			while (startPositions.length > 0  &&  startPositions[0].word == w) {
+			while (startPositions.length > 0  &&  startPositions[0].word === w) {
 				startPos = startPositions.shift();
 				refStack.unshift(startPos.refId);
 
-				if (section.startWord == w) {
+				if (section.startWord === w) {
 					// there is already a section starting at this word
 					// how?  either one finished at the previous word, or more than one ref starts here
 					section.refId = startPos.refId;
@@ -203,26 +256,29 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 				}
 			}
 
-			var endPos = null;
+			endPos = null;
 			// remove any refs that end at this pos from the "stack"
-			while (endPositions.length > 0  &&  endPositions[0].word == w) {
+			while (endPositions.length > 0  &&  endPositions[0].word === w) {
 				endPos = endPositions.shift();
-				var newRefStack = [];
+				newRefStack = [];
 				while (refStack.length) {
-					var ref = refStack.shift();
-					if (ref != endPos.refId) newRefStack.push(ref);
+					ref = refStack.shift();
+					if (ref !== endPos.refId) {
+						newRefStack.push(ref);
+					}
 				}
 				refStack = newRefStack;
 
 				// a reference ends here
 				section.endWord = w;
 				section.ending = (section.ending || endPos.ending) || false;
-				if (endPos.ending)
+				if (endPos.ending) {
 					section.refNums.push(endPos.refId);
-				if (endPositions.length == 0  ||  endPositions[0].word != w) {
+				}
+				if (endPositions.length === 0  ||  endPositions[0].word !== w) {
 					// no more to come
 					sections.push(section);
-					if (w == words.length) {
+					if (w === words.length) {
 						// this is the last word => no more sections
 						section = null;
 					} else {
@@ -236,42 +292,30 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 			section.endWord = words.length;
 			sections.push(section);
 		}
-//console.log("sections: " + sections.toSource());
 
-		for (var i = 0; i < sections.length; ++i) {
-			var section = sections[i];
+		for (i = 0; i < sections.length; ++i) {
+			section = sections[i];
 
-			var wordString =
+			wordString =
 				(section.starting ? "" : " ") +
 				words.slice(section.startWord - 1, section.endWord).join(" ") +
 				(section.ending ? "" : " ");
 
-			if (section.refId != null) {
-				var ref = refs[section.refId];
-				var a = document.createElement("a");
+			if (section.refId !== null) {
+				ref = refs[section.refId];
+				a = document.createElement("a");
 				a.className = (section.refCount > 1 ? "annotation multiple" : "annotation");
 				a.href = "#ref=" + ref.recordID;
 				a.title = ref.title;
 				a.name = "ref" + ref.recordID;
 				a.setAttribute("annotation-id", ref.recordID);
-				a.onclick = function() { highlightAnnotation(this.getAttribute("annotation-id")); return false; };
+				a.onclick = function () {
+					highlightAnnotation(this.getAttribute("annotation-id"));
+					return false;
+				};
 				a.innerHTML = wordString;
 				newElements.push(a);
 
-/*
-				for (var r = 0; r < section.refNums.length; ++r) {
-					var ref = refs[section.refNums[r]];
-					var a = document.createElement("a");
-					a.className = "annotation superscript";
-					a.href = "#ref" + (section.refNums[r] + 1);
-					a.title = ref.title;
-					a.onclick = function() { highlightAnnotation(this.getAttribute("annotation-id")); };
-					var s = a.appendChild(document.createElement("sup"));
-					s.innerHTML = "[" + (section.refNums[r] + 1) + "]";
-					//s.innerHTML = "";
-					newElements.push(a);
-				}
-*/
 			} else {
 				newElements.push(document.createTextNode(wordString));
 			}
@@ -279,10 +323,10 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 		}
 
 		// replace elem with newElements
-		var parentElem = elem.parentNode;
+		parentElem = elem.parentNode;
 		parentElem.replaceChild(newElements[0], elem);
-		for (var i = 1; i < newElements.length; ++i) {
-			parentElem.insertBefore(newElements[i], newElements[i-1].nextSibling);
+		for (i = 1; i < newElements.length; ++i) {
+			parentElem.insertBefore(newElements[i], newElements[i - 1].nextSibling);
 		}
 
 		// now update currentRefs to reflect what happened within this text node
@@ -292,13 +336,17 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 		}
 		// remove refs which end here
 		if (currentRefs.length  &&  myEndingRefs.length) {
-			var newState = [];
-			for (var i = 0; i < currentRefs.length; ++i) {
-				var remove = false;
-				for (var j = 0; j < myEndingRefs.length; ++j) {
-					if (currentRefs[i] == myEndingRefs[j]) remove = true;
+			newState = [];
+			for (i = 0; i < currentRefs.length; ++i) {
+				remove = false;
+				for (j = 0; j < myEndingRefs.length; ++j) {
+					if (currentRefs[i] === myEndingRefs[j]) {
+						remove = true;
+					}
 				}
-				if (! remove) newState.push(currentRefs[i]);
+				if (! remove) {
+					newState.push(currentRefs[i]);
+				}
 			}
 			currentRefs = newState;
 		}
@@ -309,15 +357,20 @@ function transformTextNode (elem, refs, startingRefs, endingRefs, wordOffset) {
 
 
 function alignImages() {
-	var PAD = 16;	// magic: top margin / padding of annotation thumbnails
-	$(".content-right div.annotation-img").each(function() {
-		var $annotation = $("#tei a[annotation-id=" + $(this).attr("annotation-id") + "]");
+	var PAD = 16,	// magic: top margin / padding of annotation thumbnails
+		$annotation,
+		textpos,
+		imgpos,
+		delta;
+
+	$(".content-right div.annotation-img").each(function () {
+		$annotation = $("#tei a[annotation-id=" + $(this).attr("annotation-id") + "]");
 		if ($annotation.parent().parent().is(":visible")) {
 			$(this).css("margin-top", "0px");
 			$(this).show();
-			var textpos = $annotation.offset().top;
-			var imgpos = $(this).offset().top;
-			var delta = textpos - (imgpos + PAD);
+			textpos = $annotation.offset().top;
+			imgpos = $(this).offset().top;
+			delta = textpos - (imgpos + PAD);
 			if (delta > 0) {
 				$(this).css("margin-top", delta + "px");
 			}
@@ -329,14 +382,14 @@ function alignImages() {
 
 
 function setupPageControls() {
-	$("a.entry-prev-page-link").click(function() {
-		$("#tei div:has(~ div:visible):last").each(function() {
+	$("a.entry-prev-page-link").click(function () {
+		$("#tei div:has(~ div:visible):last").each(function () {
 			showSection($("#tei div").index(this) + 1);
 		});
 		return false;
 	});
-	$("a.entry-next-page-link").click(function() {
-		$("#tei div:visible ~ div:first").each(function() {
+	$("a.entry-next-page-link").click(function () {
+		$("#tei div:visible ~ div:first").each(function () {
 			showSection($("#tei div").index(this) + 1);
 		});
 		return false;
@@ -347,15 +400,20 @@ function showSection(i) {
 	if (i === "all") {
 		YAHOO.util.History.navigate("page", "all");
 	} else {
-		YAHOO.util.History.multiNavigate({"page": ""+i, "ref": ""});
+		YAHOO.util.History.multiNavigate({"page": "" + i, "ref": ""});
 	}
 }
 
-function highlightAnnotation (id) {
-	YAHOO.util.History.navigate("ref", ""+id);
+function highlightAnnotation(id) {
+	YAHOO.util.History.navigate("ref", "" + id);
 }
 
-YAHOO.util.Event.onDOMReady(function() {
+YAHOO.util.Event.onDOMReady(function () {
+
+	var initPage,
+		initAnnotation,
+		$link,
+		$section;
 
 	function _showSection(i) {
 		if (i) {
@@ -364,13 +422,13 @@ YAHOO.util.Event.onDOMReady(function() {
 				$("#tei div").show();
 				$("a.entry-prev-page-link").add("a.entry-next-page-link").hide();
 			} else {
-				$("#tei div").hide().eq(i-1).show();
-				if (i == 1) {
+				$("#tei div").hide().eq(i - 1).show();
+				if (i === 1) {
 					$("a.entry-prev-page-link").hide();
 				} else {
 					$("a.entry-prev-page-link").show();
 				}
-				if (i == $("#tei div").length) {
+				if (i === $("#tei div").length) {
 					$("a.entry-next-page-link").hide();
 				} else {
 					$("a.entry-next-page-link").show();
@@ -383,7 +441,7 @@ YAHOO.util.Event.onDOMReady(function() {
 	function _highlightAnnotation(id) {
 		if (id) {
 			$("#tei a.annotation.highlighted").removeClass("highlighted");
-			$link = $("#tei a[annotation-id="+id+"]");
+			$link = $("#tei a[annotation-id=" + id + "]");
 			$link.addClass("highlighted");
 			$section = $link.parent().parent();
 			if (! $section.is(":visible")) {
@@ -391,8 +449,6 @@ YAHOO.util.Event.onDOMReady(function() {
 			}
 		}
 	}
-
-	var initPage, initAnnotation,
 
 	initPage = YAHOO.util.History.getBookmarkedState("page");
 	initAnnotation = YAHOO.util.History.getBookmarkedState("ref");
@@ -404,14 +460,14 @@ YAHOO.util.Event.onDOMReady(function() {
 		initAnnotation = "";
 	}
 
-	YAHOO.util.History.register("page", initPage, function(state) {
+	YAHOO.util.History.register("page", initPage, function (state) {
 		_showSection(state);
 	});
-	YAHOO.util.History.register("ref", initAnnotation, function(state) {
+	YAHOO.util.History.register("ref", initAnnotation, function (state) {
 		_highlightAnnotation(state);
 	});
 
-	YAHOO.util.History.onReady(function() {
+	YAHOO.util.History.onReady(function () {
 		_showSection(YAHOO.util.History.getCurrentState("page"));
 		_highlightAnnotation(YAHOO.util.History.getCurrentState("ref"));
 	});
@@ -419,7 +475,7 @@ YAHOO.util.Event.onDOMReady(function() {
 	try {
 		YAHOO.util.History.initialize("yui-history-field", "yui-history-iframe");
 	} catch (e) {
-		console.log("history manager init failed");
+		alert("history manager init failed");
 	}
 
 	setupPageControls();
