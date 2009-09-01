e6e44cfbaf74e297c64b54d000bb66500563073b
Kim Jackson <kjackson@acl.arts.usyd.edu.au>
2009-09-01 14:46:34 +1000

    Updated deployment generator script.

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -5,7 +5,12 @@ PIPELINE=http://heuristscholar.org/cocoon/relbrowser-kj
 
 
 # copy files
-echo "select file_id, file_nonce from files;" | mysql -s -u readonly -pmitnick heuristdb-dos | while read id nonce; do cp /var/www/htdocs/uploaded-heurist-files/dos/$id files/full/$nonce; done
+echo "select file_id, file_nonce from files;" | mysql -s -u readonly -pmitnick heuristdb-dos | \
+while read id nonce; do
+	if [[ ! -e files/full/$nonce ]]; then
+		cp /var/www/htdocs/uploaded-heurist-files/dos/$id files/full/$nonce;
+	fi
+done
 
 # generate resized images
 echo "select file_nonce from files where file_mimetype like 'image%';" | mysql -s -u readonly -pmitnick heuristdb-dos | \
@@ -27,10 +32,20 @@ while read nonce; do
 	fi
 done
 
-# generate pages for all appropriate records
-echo "select rec_id from records where rec_type in (153,151,103,74,91,152,98) and if (rec_type = 91, rd_val in ('Occupation', 'Type'), 1);" | mysql -s -u readonly -pmitnick heuristdb-dos | while read id; do wget --no-cache -O item/$id $PIPELINE/item/$id; done
+# generate pages, previews for all appropriate records
+echo "select rec_id from records left join rec_details on rd_rec_id = rec_id and rd_type = 591 where rec_type in (153,151,103,74,91,152,98) and if (rec_type = 91, rd_val in ('Occupation', 'Type'), 1);" | mysql -s -u readonly -pmitnick heuristdb-dos | \
+while read id; do
+	wget --no-cache -O item/$id $PIPELINE/item/$id;
+	wget --no-cache -O preview/$id $PIPELINE/preview/$id;
+done
 
-grep -r 'preview-[0-9]' item | perl -pe 's/.*preview-(\d+(c\d+)?).*/\1/' | sort | uniq | while read id; do wget --no-cache -O preview/$id $PIPELINE/preview/$id; done
+# generate previews for all records in all necessary contexts
+grep -r 'preview-[0-9]' item | perl -pe 's/.*preview-(\d+(c\d+)?).*/\1/' | sort | uniq | \
+while read id; do
+	if [[ ! -e preview/$id ]]; then
+		wget --no-cache -O preview/$id $PIPELINE/preview/$id;
+	fi
+done
 
 # generate popups for all multimedia records
 echo "select rec_id from records where rec_type = 74;" | mysql -s -u readonly -pmitnick heuristdb-dos | while read id; do wget --no-cache -O popup/$id $PIPELINE/popup/$id; done
@@ -55,8 +70,23 @@ php entities-json.php Map > browse/maps.js
 php entities-json.php Term > browse/subjects.js
 php entities-json.php Role > browse/roles.js
 
+wget --no-cache -O browse/artefacts $PIPELINE/browse/artefacts
+wget --no-cache -O browse/buildings $PIPELINE/browse/buildings
+wget --no-cache -O browse/events $PIPELINE/browse/events
+wget --no-cache -O browse/natural $PIPELINE/browse/natural
+wget --no-cache -O browse/organisations $PIPELINE/browse/organisations
+wget --no-cache -O browse/people $PIPELINE/browse/people
+wget --no-cache -O browse/places $PIPELINE/browse/places
+wget --no-cache -O browse/structures $PIPELINE/browse/structures
+wget --no-cache -O browse/entries $PIPELINE/browse/entries
+wget --no-cache -O browse/maps $PIPELINE/browse/maps
+wget --no-cache -O browse/subjects $PIPELINE/browse/subjects
+wget --no-cache -O browse/roles $PIPELINE/browse/roles
+
+wget --no-cache -O search/search_template.html $PIPELINE/search
+
 # generate pages quietly and track progress at 1 minute intervals
 #rm progress
 #while [ 1 ]; do ls -1 item/ | wc -l >> progress; sleep 60; done &
-#echo "select rec_id from records where rec_type in (153,151,103,74,91,152,98);" | mysql -s -u readonly -pmitnick heuristdb-dos | while read id; do wget --no-cache -O item/$id $PIPELINE/item/$id 2>/dev/null; done &
+#echo "select rec_id from records left join rec_details on rd_rec_id = rec_id and rd_type = 591 where rec_type in (153,151,103,74,91,152,98) and if (rec_type = 91, rd_val in ('Occupation', 'Type'), 1);" | mysql -s -u readonly -pmitnick heuristdb-dos | while read id; do wget --no-cache -O item/$id $PIPELINE/item/$id 2>/dev/null; done &
 
