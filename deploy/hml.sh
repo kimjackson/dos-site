a6ec3e764755746787f9562d66f18a5d990dfdad
Kim Jackson <kjackson@acl.arts.usyd.edu.au>
2009-10-28 10:09:59 +1100

    Updated HML query.

--- deploy/hml.sh
+++ deploy/hml.sh
@@ -1,6 +1,14 @@
-REPO=.
+REPO=repo
 HEURIST=../heurist-kj
-echo "select rec_id from records left join rec_details on rd_rec_id = rec_id and rd_type = 591 where rec_type in (153,151,103,74,91,152,98) and if (rec_type = 91, rd_val in ('Occupation', 'Type'), 1) and rec_id not in (2674,2675);" | mysql -s -u readonly -pmitnick heuristdb-dos | \
+ALL_ITEMS_QUERY="select rec_id
+                   from records
+              left join rec_details rt on rt.rd_rec_id = rec_id and rt.rd_type = 591
+              left join rec_details ilt on ilt.rd_rec_id = rec_id and ilt.rd_type = 618
+                  where rec_type in (153,151,103,74,91,152,98,168)
+                    and if (rec_type = 91, rt.rd_val in ('Occupation', 'Type'), 1)
+                    and if (rec_type = 168, ilt.rd_val = 'image', 1)
+                    and rec_id not in (2674,2675);"
+echo $ALL_ITEMS_QUERY | mysql -s -u readonly -pmitnick heuristdb-dos | \
 while read id; do
 	if [[ ! -e $REPO/hml/$id.xml ]]; then
 		php $HEURIST/php/hml.php -instance dos -depth 2 -q id:$id > $REPO/hml/$id.xml
