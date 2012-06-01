efcd776c774c7aeb58240e32134ac7e1026b713c
Steve White <stephenawhite@hotmail.com>
2012-06-01 18:46:24 +1000

    Static version.

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -9,7 +9,7 @@
 # make sure we go direct to the server!
 unset http_proxy
 
-PIPELINE=http://heuristscholar.org/cocoon/relbrowser-kj
+PIPELINE=http://localhost:8888/dos-static-2012-06-01
 
 REPO=repo
 
@@ -286,38 +286,38 @@ wget --no-cache -O browse.html $PIPELINE/browse
 chmod +x search/search.cgi
 
 #######
-##DONE with the generation this should now be a working site at http://heuristscholar.org/dos-static-2011-05-20
+##DONE with the generation this should now be a working site at http://heuristscholar.org/dos-static-2012-06-01
 #######ssh
 
 #COPY the files up to the production server into a new directory
 #su as kjackson to run this command
-rsync -av about.html artefact audio boxy-ie.css boxy.css browse building citation config.xml contact.html contact.php contribute.html contributor copyright.html entry event faq.html image images index.html item jquery js kml map natural_feature organisation person place popup preview recaptcha role search search.css structure style.css subject swf tiles timeline timemap.js video kimj@dos-web-prd-1.ucc.usyd.edu.au:/var/www/dos-2012-02-24/
+rsync -av about.html artefact audio boxy-ie.css boxy.css browse building citation config.xml contact.html contact.php contribute.html contributor copyright.html entry event faq.html image images index.html item jquery js kml map natural_feature organisation person place popup preview recaptcha role search search.css structure style.css subject swf tiles timeline timemap.js video kimj@dos-web-prd-1.ucc.usyd.edu.au:/var/www/dos-2012-06-01/
 
 #sync the uploaded files
 rsync -av ../dos-static-2009-10-22/files/ kimj@dos-web-prd-1.ucc.usyd.edu.au:/var/www/files/
 
-rsync -av recaptcha kimj@dos-web-prd-1.ucc.usyd.edu.au:/var/www/dos-2011-05-20/
+rsync -av recaptcha kimj@dos-web-prd-1.ucc.usyd.edu.au:/var/www/dos-2012-06-01/
 
 ##########################
 ## on production server:##
 ##########################
 cd /var/www/dos-2011-05-20
 #replace dynamic links on built version to the relative links for the static production version
-# old way gets Arg list too long error		perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-02-24/../' item/*
+# old way gets Arg list too long error		perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-06-01/../' item/*
 grep -rl dos-static-2012-02-24 item |\
 while read filename; do
-	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-02-24/../' $filename;
+	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-06-01/../' $filename;
 done
 
-# old way gets Arg list too long error		perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-02-24/../' popup/*
-grep -rl dos-static-2012-02-24 popup |\
+# old way gets Arg list too long error		perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-06-01/../' popup/*
+grep -rl dos-static-2012-06-01 popup |\
 while read filename; do
-	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-02-24/../' $filename;
+	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-06-01/../' $filename;
 done
 
 grep -rl heurist preview |\
 while read filename; do
-	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-02-24/http:\/\/dictionaryofsydney.org/' $filename;
+	perl -pi -e 's/http:\/\/heuristscholar.org\/dos-static-2012-06-01/http:\/\/dictionaryofsydney.org/' $filename;
 done
 
 #change the google key to the production key
@@ -337,12 +337,12 @@ cd /var/www/
 # hook this into test  (make sure this link sticks,
 # may have TO DELETE the link first, "rm test")
 ##########################################################################################
-ln -s /var/www/dos-2012-02-24 test
+ln -s /var/www/dos-2012-06-01 test
 
 # once the new version is tested then log in to the server same as before and
 # change the dos link to point to the new build by running this command to go LIVE!
 # note that again you may have to remove the link first "rm dos"
-#ln -fs /var/www/dos-2012-02-24 dos
+#ln -fs /var/www/dos-2012-06-01 dos
 
 # you should also move the previous link "rm previous" then
 # ln -s /var/www/dos-(yyyy-mm-dd formatted date of previous build goes here)
